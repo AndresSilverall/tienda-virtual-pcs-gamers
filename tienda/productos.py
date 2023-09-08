d22f4ecbf9b0ad11b21714c4fe2c5ceaf53371dc
Andres F. Silvera <andresfsilverall0109@gmail.com>
2023-09-07 19:40:05 -0500

    modelo productos agregado

---
+++ tienda/productos.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.db
+
+
+# Create your models here.
+class Productos(models.Model):
+    nombre = models.CharField(max_length=50, blank=False, null=False)
+    descripcion = models.TextField(max_length=800, null=False)
+    precio = models.FloatField(verbose_name="precio producto", null=False)
+    imagen = models.ImageField(upload_to="media/productos")
+
+    def __str__(self):
+        return self.nombre
+    
+
+    @property
+    def pass_():
+        pass
+    
+
+    class Meta:
+        ordering = ["nombre"]
+        verbose_name = "Mis productos"
